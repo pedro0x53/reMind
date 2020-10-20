594b24863fe709bdb297859a8c10ce2ae0d46d7d
Pedro Sousa <pedrosousa2708@gmail.com>
2020-10-20 08:01:02 -0300

    Initial Commit

---
+++ FlashCards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashCards
+//
+//  Created by Pedro Sousa on 20/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
